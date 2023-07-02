012773d17a9707663bc00bfa76e357ba97353a91
Jhonni10 <jhonieripianoscue@gmail.com>
2023-07-02 18:47:03 -0500

    creando el constructor de la clase casillaPrincipal

--- src/batallaNaval/CasillaPrincipal.java
+++ src/batallaNaval/CasillaPrincipal.java
@@ -1,4 +1,35 @@
 package batallaNaval;
 
-public class CasillaPrincipal {
+import javax.swing.*;
+import java.awt.*;
+
+public class CasillaPrincipal extends JButton{
+
+    private int fila, columna, resultadoDeTiro, insertarElBarco;
+    private String tipoDeBarco;
+    private ImageIcon imageHundido, imagenNuevoTamanho;
+    private Image imagenOtroTamanho;
+    private ImageIconToImage imageTransformada;
+    private boolean tieneBarco, fueImpactada;
+
+    /**
+     * Constructor of CasillaPrincipal class
+     * @param fila
+     * @param columna
+     */
+
+    public CasillaPrincipal(int fila, int columna) {
+        this.fila = fila;
+        this.columna = columna;
+        this.setPreferredSize(new Dimension(46, 46));
+        setBackground(new Color(24, 90, 219));
+
+        imageTransformada = new ImageIconToImage();
+
+        resultadoDeTiro = 0;
+        insertarElBarco = 0;
+        tieneBarco = false;
+        fueImpactada = false;
+    }
+
 }
