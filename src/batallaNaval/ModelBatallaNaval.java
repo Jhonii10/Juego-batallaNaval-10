f7191e754ba1e3839a2409bbf7473664ee2142b6
MoonLoveless <lexyls523@gmail.com>
2023-07-03 11:48:50 -0500

    Creando la funcion que determina los disparos

--- src/batallaNaval/ModelBatallaNaval.java
+++ src/batallaNaval/ModelBatallaNaval.java
@@ -23,5 +23,60 @@ public class ModelBatallaNaval {
         ganarUsuario = false;
     }
 
+    /**
+     * This function is to shoot at a square
+     *
+     * @param casilla
+     */
+
+     public void dispararACasillaUsuario(CasillaPrincipal casilla) {
+        casilla.setFueImpactada(true);
+        if (casilla.getTieneBarco()) {
+        String tipoDeBarco = casilla.getTipoDeBarco();
+        
+        if (tipoDeBarco.equals("portaviones")) {
+            if (portavionIA.get(0).getFueImpactada() && portavionIA.get(1).getFueImpactada() && portavionIA.get(2).getFueImpactada() && portavionIA.get(3).getFueImpactada()) {
+                portavionIA.forEach(c -> c.determinarPrecision(6));
+            } else {
+                casilla.determinarPrecision(5);
+            }
+        } else if (tipoDeBarco.equals("submarino")) {
+            boolean submarino1Impactado = submarinoIA1.stream().allMatch(CasillaPrincipal::getFueImpactada);
+            boolean submarino2Impactado = submarinoIA2.stream().allMatch(CasillaPrincipal::getFueImpactada);
+            
+            if (submarino1Impactado && submarinoIA1.contains(casilla)) {
+                submarinoIA1.forEach(c -> c.determinarPrecision(6));
+            } else if (submarino2Impactado && submarinoIA2.contains(casilla)) {
+                submarinoIA2.forEach(c -> c.determinarPrecision(6));
+            } else {
+                casilla.determinarPrecision(5);
+            }
+        } else if (tipoDeBarco.equals("destructor")) {
+            boolean destructor1Impactado = destructorIA1.stream().allMatch(CasillaPrincipal::getFueImpactada);
+            boolean destructor2Impactado = destructorIA2.stream().allMatch(CasillaPrincipal::getFueImpactada);
+            boolean destructor3Impactado = destructorIA3.stream().allMatch(CasillaPrincipal::getFueImpactada);
+            
+            if (destructor1Impactado && destructorIA1.contains(casilla)) {
+                destructorIA1.forEach(c -> c.determinarPrecision(6));
+            } else if (destructor2Impactado && destructorIA2.contains(casilla)) {
+                destructorIA2.forEach(c -> c.determinarPrecision(6));
+            } else if (destructor3Impactado && destructorIA3.contains(casilla)) {
+                destructorIA3.forEach(c -> c.determinarPrecision(6));
+            } else {
+                casilla.determinarPrecision(5);
+            }
+        } else if (tipoDeBarco.equals("fragata")) {
+            casilla.determinarPrecision(6);
+        }
+        
+        puntosUsuario++;
+        setTurnoDeLaIA(false);
+    } else {
+        casilla.determinarPrecision(7);
+        setTurnoDeLaIA(true);
+    }
+}
+
+
 
     }
